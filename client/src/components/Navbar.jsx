5082346ca1b8eb644386d36f95e3e2169237f4c1
kethnal <kaseyvaldez@hotmail.com>
2023-02-03 23:55:55 -0800

    Make scss variables for tolours, gonna actually start to look at tailwind tho lol

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import './Navbar.scss';
+
+export default function Navbar() {
+  return (
+    <div className="Navbar">
+      <h3>THE LAST OF THEM</h3>
+      <h3 className='user'>Logged in as: Joel</h3>
+    </div>
+  );
+}
