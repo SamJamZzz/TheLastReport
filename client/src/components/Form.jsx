c5edbfd041ff7d8c2f0b4b07e37fb7826788681e
kethnal <kaseyvaldez@hotmail.com>
2023-02-04 16:15:53 -0800

    Update checkboxes in form component

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,5 +1,7 @@
 import TextField from '@mui/material/TextField';
 import Slider from '@mui/material/Slider';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 export default function Form() {
   const infectedTypes = ["Stalkers", "Runners", "Clickers", "Shamblers", "Bloaters", "Rat King"];
@@ -7,8 +9,7 @@ export default function Form() {
   const infectedCheckboxList = infectedTypes.map(type => {
     return (
       <div key={infectedTypes.indexOf(type)}>
-        <input type="checkbox" id={type} value={type} className='m-1'/>
-        <label for={type}>{type}</label>
+        <FormControlLabel control={<Checkbox defaultUnchecked />} label={type} />
       </div>
     )
   });
