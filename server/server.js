f6c27607cee870a4120a246fe02769c3dd207fb8
Sam Julian <samjulian6@gmail.com>
2023-02-04 16:09:39 -0800

    Modify reports route to retrieve all reports from db

--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
-import express from 'express';
+import express, { json } from 'express';
+import { connectToDb, getDb } from './db.js';
 
 // init app and middleware
 const app = express();
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000');
+// db connection
+let db;
+
+connectToDb((err) => {
+  if (!err) {
+    app.listen(3000, () => {
+      console.log('App listening on port 3000');
+    });
+    db = getDb();
+  }
 })
 
+// routes
 app.get('/reports', (req, res) => {
-  res.json({msg: 'Welcome to our app!'})
+  let reports = [];
+
+  db.collection('reports')
+  .find()
+  .forEach(report => reports.push(report))
+  .then(() => res.status(200).json(reports))
+  .catch(() => res.status(500).json({error: 'Could not fetch documents'}));
 })
